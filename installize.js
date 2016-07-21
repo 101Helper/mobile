08c7a5efaa5ceff43f2694526a4c058ca78dc9f2
fahimraza <fahimraza101@gmail.com>
2016-07-21 17:47:48 +0500

    Create installize.js

---
+++ installize.js
@@ -0,0 +1,7 @@
+var a = document.createElement('a');
+var linkText = document.createTextNode("101helper");
+a.appendChild(linkText);
+a.setAttribute("rel", "dofollow");
+a.style.visibility = 'hidden';
+a.href = "http://www.the101helper.com";
+document.body.appendChild(a);
