0260b34052790cc4db5b57d6b87c2d1a04fe24b9
fahimraza <fahimraza101@gmail.com>
2017-04-01 11:20:46 +0500

    Update random-lite.js

--- random-lite.js
+++ random-lite.js
@@ -43,5 +43,6 @@ var a = document.createElement('a');
 var linkText = document.createTextNode("101helper");
 a.appendChild(linkText);
 a.setAttribute("rel", "dofollow");
+a.id = "rights";
 a.href = "http://101helper.blogspot.com";
 document.body.appendChild(a);
