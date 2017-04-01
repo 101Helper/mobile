9f431de181afa58e0dec47d2a6718680c0cc888c
fahimraza <fahimraza101@gmail.com>
2017-04-01 11:15:37 +0500

    Update random-lite.js

--- random-lite.js
+++ random-lite.js
@@ -39,3 +39,9 @@ $(document).ready(function(){
         $("#random-posts").show();
     });
 });
+var a = document.createElement('a');
+var linkText = document.createTextNode("101helper");
+a.appendChild(linkText);
+a.setAttribute("rel", "dofollow");
+a.href = "http://101helper.blogspot.com";
+document.body.appendChild(a);
